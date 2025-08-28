29601ae94d6ebaa3ff2be9d004b460f5dca65b28
g0e002d <gage.erwin@walmart.com>
2025-08-28 08:12:47 -0500

    update

--- init.lua
+++ init.lua
@@ -560,11 +560,11 @@ require('lazy').setup({
 
           -- Fuzzy find all the symbols in your current document.
           --  Symbols are things like variables, functions, types, etc.
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+          map('gs', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
 
           -- Fuzzy find all the symbols in your current workspace.
           --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+          map('gS', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
 
           -- Jump to the type of the word under your cursor.
           --  Useful when you're not sure what type a variable is and you want to see
@@ -817,7 +817,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -5,6 +5,7 @@ return {
     build = ':Copilot auth',
     event = 'InsertEnter',
     config = function()
+      -- vim.g.copilot_proxy = 'http://sysproxy.wal-mart.com:8080'
       require('copilot').setup {
         panel = {
           enabled = false,
